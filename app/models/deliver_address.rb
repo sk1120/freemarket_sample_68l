73f7565501bac657bb23860c78ff42efa1773b3e
masa2946 <n.syt.m2946@gmail.com>
2020-02-14 20:38:39 +0900

    wizard_fix

--- app/models/deliver_address.rb
+++ app/models/deliver_address.rb
@@ -1,3 +1,4 @@
 class DeliverAddress < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  validates :zip_code, :prefecture, :city, :address1, presence: true
 end
--- db/migrate/20200213115148_create_deliver_addresses.rb
+++ db/migrate/20200213115148_create_deliver_addresses.rb
@@ -2,10 +2,10 @@ class CreateDeliverAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :deliver_addresses do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :family_name, null: false
-      t.string :first_name, null: false
-      t.string :family_name_kana, null: false
-      t.string :first_name_kana, null: false
+      t.string :family_name
+      t.string :first_name
+      t.string :family_name_kana
+      t.string :first_name_kana
       t.integer :zip_code, null: false
       t.string :prefecture, null: false
       t.string :city, null: false
