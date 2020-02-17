c4ae6dfc17bf816ec1bfcdd1804ce038c754a3a3
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-02-17 10:39:32 +0900

    ログアウト画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "mainpages#index"
+  resources :mainpages do
+    collection do
+      get 'logout'
+    end
+  end
 end
