8ff0cf18e315736d024af46f2507132e62b157f9
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-02-20 17:09:30 +0900

    ドロップゾーンを新規functionに追加
    ＋scssを@importに修正

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,9 +1,4 @@
 $(function(){
-  Dropzone.autoDiscover = false;
-  $(".dropzone").dropzone({
-      maxFilesize: 1,
-      addRemoveLinks: true
-  );
   $('#price_calc').on('input', function(){   
     var data = $('#price_calc').val(); 
     var profit = Math.round(data * 0.9)  
@@ -18,4 +13,12 @@ $(function(){
     $('.main__content__price__box__commission__bar').html('');
     }
   })
+})
+
+$(function(){
+  Dropzone.autoDiscover = false;
+  $(".dropzone").dropzone({
+      maxFilesize: 1,
+      addRemoveLinks: true
+  });
 })
