fd3c24baec2a9d422ab4fd649936743870bec93b
sk1120 <b100800sk@gmail.com>
2020-02-25 20:58:36 +0900

    コントローラー修正

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  
+  def index
+    # @categories = Category.all.where(ancestry: nil)
+  end
+
+  def show
+    # @category = Category.find(params[:id])
+    # @items = Item.find_by(category_child_id:@category.id)
+  end
+
+end
