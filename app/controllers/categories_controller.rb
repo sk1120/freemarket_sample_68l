182ac29495fb028459e4eaa0ad71a18e4e0fafbd
sk1120 <b100800sk@gmail.com>
2020-02-25 20:10:51 +0900

    不要な記述の削除

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
+  
   def index
+    @categories = Category.all.where(ancestry: nil)
   end
 
   def show
+    # @items = Category.find(params[:id])
   end
+
+
+  private
+
+  # def category_params
+  #   params.require(:category).permit(:content, :image).merge(category_id: current_category.id)
+  # end
+
 end
