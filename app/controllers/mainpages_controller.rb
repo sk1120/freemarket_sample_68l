8c74c2bde05a44839be52aa7a0eedfbb84009b6b
sk1120 <b100800sk@gmail.com>
2020-02-26 21:55:26 +0900

    トップページに特定カテゴリ(レディース)商品を表示

--- app/controllers/mainpages_controller.rb
+++ app/controllers/mainpages_controller.rb
@@ -1,5 +1,11 @@
 class MainpagesController < ApplicationController
   def index
+    @category = Category.find(1)
+    @items = Item.where(category_parent_id:@category.id)
+  end
+
+  def show
+    @items = Item.where(category_parent_id:@category.id)
   end
 
 end
