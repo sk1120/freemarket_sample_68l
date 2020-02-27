25e1a4f79caf49e862d3e7015e632f49141d60e5
sk1120 <b100800sk@gmail.com>
2020-02-27 19:03:36 +0900

    新規投稿商品を表示

--- app/controllers/mainpages_controller.rb
+++ app/controllers/mainpages_controller.rb
@@ -1,7 +1,8 @@
 class MainpagesController < ApplicationController
   def index
     @category = Category.find(1)
-    @items = Item.where(category_parent_id:@category.id).limit(3)
+    @items = Item.where(category_parent_id:@category.id).order("created_at DESC").limit(3)
+    @itemsnew = Item.all.order("created_at DESC").limit(3)
   end
 
   def show
