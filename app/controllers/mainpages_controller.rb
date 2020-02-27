f23eb45abc6d01aabf87ae6a428a4ca9378a061a
sk1120 <b100800sk@gmail.com>
2020-02-27 12:23:37 +0900

    トップページの商品表示数の上限設定

--- app/controllers/mainpages_controller.rb
+++ app/controllers/mainpages_controller.rb
@@ -1,7 +1,7 @@
 class MainpagesController < ApplicationController
   def index
     @category = Category.find(1)
-    @items = Item.where(category_parent_id:@category.id)
+    @items = Item.where(category_parent_id:@category.id).limit(3)
   end
 
   def show
