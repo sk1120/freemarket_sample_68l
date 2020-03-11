c7a70dde994addd13257880aaa1162d5eed66784
masa2946 <n.syt.m2946@gmail.com>
2020-03-11 23:22:59 +0900

    カテゴリー部分のパンくず機能削除

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -19,32 +19,3 @@ end
 crumb :categories do
   link "カテゴリ一覧", categories_path
 end
-
-crumb :category do |category|
-  @item = Item.find(params[:id])
-  link category, "/categories/#{@item.category_grand_child.parent.parent.id}"
-  parent :categories
-end
-
-crumb :parents do |parents|
-  @item = Item.find(params[:id])
-  link @item.category_grand_child.parent.parent.name, "/categories/#{@item.category_grand_child.parent.parent.id}"
-  parent :categories
-end
-
-crumb :child do |child|
-  @item = Item.find(params[:id])
-  link @item.category_grand_child.parent.name, "/categories/#{@item.category_grand_child.parent.id}"
-  parent :parents
-end
-
-crumb :grandchild do |grandchild|
-  @item = Item.find(params[:id])
-  link @item.category_grand_child.name, "/categories/#{@item.category_grand_child.id}"
-  parent :child
-end
-
-crumb :item do |item|
-  link item
-  parent :grandchild
-end
